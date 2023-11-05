95a3152bf96c4419cc414f14678687664f5436ba
Roy Gerstner <TuxLeon@gmail.com>
2023-11-05 19:08:38 +0100

    First init

---
+++ blink.ino
@@ -0,0 +1,19 @@
+void setup()
+{
+   pinMode(LED_BUILTIN, OUTPUT);
+   Serial.begin(115200);
+   while (!Serial)
+      ;
+}
+
+void loop()
+{
+   if (Serial.available()){
+      String inputString = Serial.readString();
+          Serial.println(inputString);
+   }
+   digitalWrite(LED_BUILTIN, HIGH);
+   delay(500);
+   digitalWrite(LED_BUILTIN, LOW);
+   delay(500);
+}
